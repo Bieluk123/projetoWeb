3ca9d8cccb28ccdf25ffa7d241c4f10835bbc794
Bieluk123 <gabriel.souzaalmeida@yahoo.com.br>
2023-12-19 22:19:09 -0300

    Adicionei algumas coisas

--- js/controller/jogoController.js
+++ js/controller/jogoController.js
@@ -29,4 +29,60 @@ class JogoController {
   deleteJogo(id) {
     this.jogos = this.jogos.filter(jogo => jogo.id !== id);
   }
+
+  // Método para preencher a tabela no HTML com os jogos existentes
+  renderTable() {
+    const tbody = document.getElementById('tbody');
+    tbody.innerHTML = '';
+
+    this.jogos.forEach(jogo => {
+      const row = document.createElement('tr');
+      row.innerHTML = `
+        <td>${jogo.id}</td>
+        <td>${jogo.nome}</td>
+        <td>${jogo.genero}</td>
+        <td>${jogo.ano}</td>
+        <td>${jogo.preco}</td>
+        <td>${jogo.nomeCompleto()}</td>
+        <td>
+          <button onclick="editarJogo(${jogo.id})">Editar</button>
+          <button onclick="excluirJogo(${jogo.id})">Excluir</button>
+        </td>
+      `;
+      tbody.appendChild(row);
+    });
+  }
+}
+
+// Função para inicializar o controlador e renderizar a tabela
+const jogoController = new JogoController();
+jogoController.renderTable();
+
+// Função para adicionar um novo jogo a partir dos dados do formulário
+function adicionarJogo() {
+  const nome = document.getElementById('inpNome').value;
+  const genero = document.getElementById('inpGenero').value;
+  const ano = document.getElementById('inpAno').value;
+  const preco = document.getElementById('inpPreco').value;
+
+  jogoController.createJogo(nome, genero, ano, preco);
+  jogoController.renderTable();
+}
+
+// Função para editar um jogo
+function editarJogo(id) {
+  const jogo = jogoController.getJogoById(id);
+  if (jogo) {
+    document.getElementById('inpCodigo').value = jogo.id;
+    document.getElementById('inpNome').value = jogo.nome;
+    document.getElementById('inpGenero').value = jogo.genero;
+    document.getElementById('inpAno').value = jogo.ano;
+    document.getElementById('inpPreco').value = jogo.preco;
+  }
+}
+
+// Função para excluir um jogo
+function excluirJogo(id) {
+  jogoController.deleteJogo(id);
+  jogoController.renderTable();
 }
--- js/model/jogo.js
+++ js/model/jogo.js
@@ -1,9 +1,13 @@
-class jogo {
+class Jogo {
   constructor(id, nome, genero, ano, preco) {
     this.id = id;
     this.nome = nome;
     this.genero = genero;
     this.ano = ano;
     this.preco = preco;
+  }
+
+  nomeCompleto() {
+    return `${this.nome} ${this.genero}`;
   }
 }
